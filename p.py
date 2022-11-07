f4534c04b2bfae5da70bf63de0b4e6453f041087
zi <zijunchen3@gmail.com>
2022-11-07 13:36:53 +0000

    p1

---
+++ p.py
@@ -0,0 +1,9 @@
+class Photo:
+    def __init__(self,gener,time,quantity):
+        self.gener=gener
+        self.time=time
+        self.quantity=quantity
+
+p1=Photo("lolita",2.30,5)
+print(p1.time)
+ 
